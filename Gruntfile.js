7b256529bcbf2891553d89082d2e65960df797fc
JKAussieSkater <JKAussieSkater@users.noreply.github.com>
2016-01-25 18:22:26 +1100

    Rearranging Gruntfile Tasks; Autoconvert "absolute paths" into "relative paths"; Gather non-text resources within `src/_processed/` folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,154 @@ module.exports = function (grunt) {
                     '-------------------------------------\n' +
                     '     Version: <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)',
 
-        // Compiles Sass files into CSS files
+        // Copies files from 3rd party sources to the `src` folder
+        // Copies files from `src` folder to `dist` folder on production
+        copy: {
+            components: {
+                files: [{
+                    expand: true,
+                    cwd: 'components/bootswatch/',
+                    src: ['superhero/**'],
+                    dest: 'src/scss/',
+                    filter: function (dest) {
+                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '');
+                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
+                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
+                        else { return true; }
+                    }
+                }]
+            },
+            bootstrap_assets: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components/bootstrap-sass/assets/',
+                    src: [
+                        'stylesheets/bootstrap/**',
+                        'stylesheets/_bootstrap.scss',
+                        'fonts/bootstrap/**',
+                        'javascripts/bootstrap.js'
+                    ],
+                    dest: 'src/',
+                    filter: function (dest) {
+                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '').replace(/^stylesheets\//, 'scss/').replace(/^scss\/_/, 'scss/').replace(/^javascripts\//, 'js/');
+                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
+                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
+                        else { return true; }
+                    },
+                    rename: function (dest, src) {
+                        return dest + src.replace(/^stylesheets\//, 'scss/').replace(/^scss\/_/, 'scss/').replace(/^javascripts\//, 'js/');
+                    }
+                }]
+            },
+            bower_components: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components/',
+                    src: [
+                        'font-awesome/scss/_**',
+                        '!font-awesome/scss/_variables.scss',
+                        'font-awesome/fonts/**',
+                        'jquery/dist/jquery.js',
+                        'html5shiv/dist/html5shiv.js',
+                        'respond/dest/respond.src.js'
+                    ],
+                    dest: 'src/',
+                    filter: function (dest) {
+                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '').replace(/^font-awesome\/(.*)\//, '$1/font-awesome/').replace(/^jquery\/dist\//, 'js/').replace(/^html5shiv\/dist\//, 'js/polyfill/').replace(/^respond\/dest\/respond.src.js/, 'js/polyfill/respond.js');
+                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
+                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
+                        else { return true; }
+                    },
+                    rename: function (dest, src) {
+                        return dest + src.replace(/^font-awesome\/(.*)\//, '$1/font-awesome/').replace(/^jquery\/dist\//, 'js/').replace(/^html5shiv\/dist\//, 'js/polyfill/').replace(/^respond\/dest\/respond.src.js/, 'js/polyfill/respond.js');
+                    }
+                }]
+            },
+            process__fontawesome: {
+                options: {
+                    process: function (content, path) {
+                        if (~path.indexOf('font-awesome.scss')) {
+                            return content.replace(/@import "/g, '@import "font-awesome/');
+                        } else if (~path.indexOf('_variables.scss')) {
+                            return content.replace(/"\.\.\/fonts"/, '"../fonts/font-awesome"');
+                        }
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components/font-awesome/scss/',
+                    src: [
+                        'font-awesome.scss',
+                        '_variables.scss'
+                    ],
+                    dest: 'src/scss/',
+                    filter: function (dest) {
+                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '').replace(/^_/, 'font-awesome/_');
+                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
+                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
+                        else { return true; }
+                    },
+                    rename: function (dest, src) {
+                        return dest + src.replace(/^_/, 'font-awesome/_');
+                    }
+                }]
+            },
+            process__html: {
+                options: {
+                    process: function (content, path) {
+                        var strDepth = '../';
+                        for (var i = path.match(/\//g).length - 2; i; i--) {
+                            strDepth = strDepth + '../';
+                        }
+
+                        // RegExp to find an opening and closing custom tag
+                        // The closing tag is mentioned twice; this helps the opening tag match with the first available closing tag (as opposed to the last found tag)
+                        return content.replace(/\/src\/_processed\//g, strDepth).replace(/<!--concat:css-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<link href="' + strDepth + 'css/master.min.css" rel="stylesheet">').replace(/<!--concat:js-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<script src="' + strDepth + 'js/master.min.js"></script>').replace(/<!--concat:polyfill-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<!--[if lt IE 9]><script src="' + strDepth + 'js/polyfill/master.min.js"></script><![endif]-->').replace(/<!--concat:css_bootswatch-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<link href="' + strDepth + 'css/master_bootswatch.min.css" rel="stylesheet">');
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/html/',
+                    src: ['**'],
+                    dest: 'src/_processed/html/',
+                    ext: '.processed.html',
+                    filter: 'isFile'
+                }]
+            },
+            gather_nontext: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: [
+                        'fonts/',
+                        'images/'
+                    ],
+                    dest: 'src/_processed/'
+                }]
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/_processed/',
+                    src: [
+                        'css/**',
+                        'fonts/**',
+                        'html/**/*.html', '!html/**/*.processed.html',
+                        'images/**',
+                        'js/**/*.min.js',
+                        '../../LICENSE',
+                        '../../README.md'
+                    ],
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        return dest + src.replace(/^(\.\.\/)+/, '');
+                    }
+                }]
+            }
+        },
+
+// Compiles Sass files into CSS files
         sass: {
             scss: {
                 options: {
@@ -131,142 +278,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // Copies files from 3rd party sources to the `src` folder
-        // Copies files from `src` folder to `dist` folder on production
-        copy: {
-            components: {
-                files: [{
-                    expand: true,
-                    cwd: 'components/bootswatch/',
-                    src: ['superhero/**'],
-                    dest: 'src/scss/',
-                    filter: function (dest) {
-                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '');
-                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
-                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
-                        else { return true; }
-                    }
-                }]
-            },
-            bootstrap_assets: {
-                files: [{
-                    expand: true,
-                    cwd: 'bower_components/bootstrap-sass/assets/',
-                    src: [
-                        'stylesheets/bootstrap/**',
-                        'stylesheets/_bootstrap.scss',
-                        'fonts/bootstrap/**',
-                        'javascripts/bootstrap.js'
-                    ],
-                    dest: 'src/',
-                    filter: function (dest) {
-                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '').replace(/^stylesheets\//, 'scss/').replace(/^scss\/_/, 'scss/').replace(/^javascripts\//, 'js/');
-                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
-                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
-                        else { return true; }
-                    },
-                    rename: function (dest, src) {
-                        return dest + src.replace(/^stylesheets\//, 'scss/').replace(/^scss\/_/, 'scss/').replace(/^javascripts\//, 'js/');
-                    }
-                }]
-            },
-            bower_components: {
-                files: [{
-                    expand: true,
-                    cwd: 'bower_components/',
-                    src: [
-                        'font-awesome/scss/_**',
-                        '!font-awesome/scss/_variables.scss',
-                        'font-awesome/fonts/**',
-                        'jquery/dist/jquery.js',
-                        'html5shiv/dist/html5shiv.js',
-                        'respond/dest/respond.src.js'
-                    ],
-                    dest: 'src/',
-                    filter: function (dest) {
-                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '').replace(/^font-awesome\/(.*)\//, '$1/font-awesome/').replace(/^jquery\/dist\//, 'js/').replace(/^html5shiv\/dist\//, 'js/polyfill/').replace(/^respond\/dest\/respond.src.js/, 'js/polyfill/respond.js');
-                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
-                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
-                        else { return true; }
-                    },
-                    rename: function (dest, src) {
-                        return dest + src.replace(/^font-awesome\/(.*)\//, '$1/font-awesome/').replace(/^jquery\/dist\//, 'js/').replace(/^html5shiv\/dist\//, 'js/polyfill/').replace(/^respond\/dest\/respond.src.js/, 'js/polyfill/respond.js');
-                    }
-                }]
-            },
-            process__fontawesome: {
-                options: {
-                    process: function (content, path) {
-                        if (~path.indexOf('font-awesome.scss')) {
-                            return content.replace(/@import "/g, '@import "font-awesome/');
-                        } else if (~path.indexOf('_variables.scss')) {
-                            return content.replace(/"\.\.\/fonts"/, '"../fonts/font-awesome"');
-                        }
-                    }
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'bower_components/font-awesome/scss/',
-                    src: [
-                        'font-awesome.scss',
-                        '_variables.scss'
-                    ],
-                    dest: 'src/scss/',
-                    filter: function (dest) {
-                        var output = grunt.task.current.data.files[0].dest + dest.replace(new RegExp('^' + this.cwd), '').replace(/^_/, 'font-awesome/_');
-                        if (!grunt.file.isFile(dest)) { console.log('Not a file: ' + dest); return false; }
-                        else if (grunt.file.exists(output)) { console.log('Already exists: ' + output); return false; }
-                        else { return true; }
-                    },
-                    rename: function (dest, src) {
-                        return dest + src.replace(/^_/, 'font-awesome/_');
-                    }
-                }]
-            },
-            process__html: {
-                options: {
-                    process: function (content, path) {
-                        var strDepth = '../';
-                        for (var i = path.match(/\//g).length - 2; i; i--) {
-                            strDepth = strDepth + '../';
-                        }
-
-                        // RegExp to find an opening and closing custom tag
-                        // The closing tag is mentioned twice; this helps the opening tag match with the first available closing tag (as opposed to the last found tag)
-                        return content.replace(/<!--concat:css-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<link href="' + strDepth + 'css/master.min.css" rel="stylesheet">').replace(/<!--concat:js-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<script src="' + strDepth + 'js/master.min.js"></script>').replace(/<!--concat:polyfill-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<!--[if lt IE 9]><script src="' + strDepth + 'js/polyfill/master.min.js"></script><![endif]-->').replace(/<!--concat:css_bootswatch-->(?:(?!<!--\/concat-->)(?:.|\n))*<!--\/concat-->/g, '<link href="' + strDepth + 'css/master_bootswatch.min.css" rel="stylesheet">');
-                    }
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/html/',
-                    src: ['**'],
-                    dest: 'src/_processed/html/',
-                    ext: '.processed.html',
-                    filter: 'isFile'
-                }]
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/_processed/',
-                    src: [
-                        'css/**/*.min.css',
-                        '../fonts/**',
-                        'html/**/*.html', '!html/**/*.processed.html',
-                        '../images/**',
-                        'js/**/*.min.js',
-                        '../../LICENSE',
-                        '../../README.md'
-                    ],
-                    dest: 'dist/',
-                    filter: 'isFile',
-                    rename: function (dest, src) {
-                        return dest + src.replace(/^(\.\.\/)+/, '');
-                    }
-                }]
-            }
-        },
-
         // Merges all CSS files, JS files, and polyfill files into single files
         // Allows for fewer HTTP requests, and faster browser load speeds
         concat: {
@@ -321,7 +332,7 @@ module.exports = function (grunt) {
                 'src/_processed/css/**/*.postcss.scss.css',
                 'src/_processed/css/**/*.postcss.scss.css.map'
             ],
-            copy_process__html: ['src/_processed/html/**/*.processed.html'],
+            copy_process__html: ['src/_processed/html/**/*.dprocessed.html'],
             processed: ['src/_processed/*'],
             processed_css: ['src/_processed/css/*'],
             processed_js: ['src/_processed/js/*'],
@@ -406,7 +417,8 @@ module.exports = function (grunt) {
     grunt.registerTask('Process-Files', [
         'sass',
         'postcss', 'clean:sass_scss',
-        'copy:process__html'
+        'copy:process__html',
+        'copy:gather_nontext'
     ]);
     grunt.registerTask('Minify-Processed', [
         'cssmin', 'clean:postcss_css', 'clean:postcss_scss',
